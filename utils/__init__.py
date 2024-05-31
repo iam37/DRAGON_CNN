b186d35c8167423e90337a7ee5a34a6d4066bb40
Jeremy Ng <jeremy.ng@yale.edu>
2024-05-31 10:41:23 -0400

    Further refinement changes to utils

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from .data_utils import load_data_dir
 from .device_utils import discover_devices
 from .tensor_utils import load_tensor
+from .model_utils import specify_dropout_rate, enable_dropout
 
-__all__: ["load_data_dir", "discover_devices", "load_tensor"]
+__all__: ["load_data_dir", "discover_devices", "load_tensor", "specify_dropout_rate", "enable_dropout"]
