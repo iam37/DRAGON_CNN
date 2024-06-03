1cb8e0e8e3ce83cc6890c81dd604c02e1745edc5
Jeremy Ng <jeremy.ng@yale.edu>
2024-06-03 14:08:26 -0400

    W&B Sweeps

---
+++ train/wandb_sweep_train.py
@@ -0,0 +1,227 @@
+# -*- coding: utf-8 -*-
+import click
+import logging
+import math
+from pathlib import Path
+from functools import partial
+
+import wandb
+
+import torch
+import torch.nn as nn
+import torch.optim as opt
+
+import kornia.augmentation as K
+
+from data_preprocessing import FITSDataset, get_data_loader
+from cnn import model_factory, model_stats, save_trained_model
+from train import create_trainer
+from utils import discover_devices, specify_dropout_rate
+
+
+# TODO: FIX THIS!!!
+
+
+# Global Sweep Configuration
+sweep_config = {
+    "method": "bayes",
+    "metrics": {"goal": "minimize", "name": "calc_loss"},
+    "parameters": {
+        "learning_rate": [0.001, 0.0001, 0.0005],
+        "momentum": [0, 1e-6, 1e-3],
+        "nesterov": [True, False],
+        "weight_decay": [0, 1e-6, 1e-12],
+        "epochs": 10,
+        "batch_size": [16, 32, 64]
+    }
+}
+
+@click.command()
+@click.option("--experiment_name", type=str, default="demo")
+@click.option(
+    "--run_id",
+    type=str,
+    default=None,
+    help="""The run id. Practically this only needs to be used
+if you are resuming a previosuly run experiment""",
+)
+@click.option(
+    "--run_name",
+    type=str,
+    default=None,
+    help="""A run is supposed to be a sub-class of an experiment.
+So this variable should be specified accordingly""",
+)
+@click.option(
+    "--model_type",
+    type=click.Choice(
+        [
+            "dragon"
+        ],
+        case_sensitive=False,
+    ),
+    default="dragon",
+)
+@click.option("--model_state", type=click.Path(exists=True), default=None)
+@click.option("--data_dir", type=click.Path(exists=True), required=True)
+@click.option(
+    "--split_slug",
+    type=str,
+    required=True,
+    help="""This specifies how the data_preprocessing is split into train/
+devel/test sets. Balanced/Unbalanced refer to whether selecting
+equal number of images from each class. xs, sm, lg, dev all refer
+to what fraction is picked for train/devel/test.""",
+)
+@click.option("--cutout_size", type=int, default=167)
+@click.option("--channels", type=int, default=3)
+@click.option(
+    "--n_workers",
+    type=int,
+    default=4,
+    help="""The number of workers to be used during the
+data_preprocessing loading process.""",
+)
+@click.option(
+    "--parallel/--no-parallel",
+    default=True,
+    help="""The parallel argument controls whether or not
+to use multiple GPUs when they are available""",
+)
+@click.option(
+    "--normalize/--no-normalize",
+    default=True,
+    help="""The normalize argument controls whether or not, the
+loaded images will be normalized using the arsinh function""",
+)
+@click.option(
+    "--crop/--no-crop",
+    default=True,
+    help="""If True, all images are passed through a cropping
+operation before being fed into the network. Images are cropped
+to the cutout_size parameter""",
+)
+def sweep_init(**kwargs):
+    # Copy and log args
+    args = {k: v for k, v in kwargs.items()}
+
+    # Discover devices
+    args["device"] = discover_devices()
+
+    # Create the model given model_type
+    cls = model_factory(args["model_type"])
+    model_args = {
+        "cutout_size": args["cutout_size"],
+        "channels": args["channels"],
+    }
+
+    if "drp" in args["model_type"].split("_"):
+        logging.info(
+            "Using dropout rate of {} in the model".format(
+                args["dropout_rate"]
+            )
+        )
+        model_args["dropout"] = "True"
+
+    model = cls(**model_args)
+    model = nn.DataParallel(model) if args["parallel"] else model
+    model = model.to(args["device"])
+
+    # Chnaging the default dropout rate if specified
+    if args["dropout_rate"] is not None:
+        specify_dropout_rate(model, args["dropout_rate"])
+
+    # Select the desired transforms
+    T = None
+    if args["crop"]:
+        T = K.CenterCrop(args["cutout_size"])
+
+    # Generate the DataLoaders and log the train/devel/test split sizes
+    splits = ("train", "devel", "test")
+    datasets = {
+        k: FITSDataset(
+            data_dir=args["data_dir"],
+            slug=args["split_slug"],
+            cutout_size=args["cutout_size"],
+            channels=args["channels"],
+            normalize=args["normalize"],
+            transforms=T,
+            split=k,
+        )
+        for k in splits
+    }
+
+    # Select the desired transforms
+    T = None
+    if args["crop"]:
+        T = K.CenterCrop(args["cutout_size"])
+
+    # Define the criterion
+    loss_dict = {
+        "nll": nn.NLLloss(),
+    }
+    criterion = loss_dict[args["loss"]]
+
+    # Log into W&B
+    wandb.login()
+
+    trainer_func = partial(train, model=model, datasets=datasets, criterion=criterion, args=args)
+    sweep_id = wandb.sweep(sweep=sweep_config, project=args["experiment_name"])
+    wandb.agent(sweep_id=sweep_id, function=trainer_func, count=10)
+
+
+def train(model, datasets, criterion, args):
+    # Initializing W&B run
+    with wandb.init(
+        project=args["experiment_name"],
+        id=args["run_id"],
+        resume="allow",
+    ) as run:
+        """Runs the training procedure using MLFlow."""
+        optimizer = opt.SGD(
+            model.parameters(),
+            lr=sweep_config.parameters.learning_rate,
+            momentum=sweep_config.parameters.momentum,
+            nesterov=sweep_config.parameters.nesterov,
+            weight_decay=sweep_config.parameters.weight_decay
+        )
+
+        # Create a DataLoader factory based on command-line args
+        loader_factory = partial(
+            get_data_loader,
+            batch_size=wandb.config.parameters.batch_size,
+            n_workers=args["n_workers"],
+        )
+
+        loaders = {k: loader_factory(v) for k, v in datasets.items()}
+        args["splits"] = {k: len(v.dataset) for k, v in loaders.items()}
+
+        # Write the parameters and model stats to W&B
+        args["calc_loss"] = criterion
+        args = {**args, **model_stats(model)}
+        wandb.log(args)
+
+        # Set up trainer
+        trainer = create_trainer(
+            model, optimizer, criterion, loaders, args["device"]
+        )
+
+        # Run trainer and save model state
+        trainer.run(loaders["train"], max_epochs=args["epochs"])
+        slug = (
+            f"{args['experiment_name']}-{args['split_slug']}-"
+            f"{run.id}"
+        )
+
+        model_path = save_trained_model(model, slug)
+        logging.info(f"Saved model to {model_path}")
+
+        # Log model as an artifact
+        wandb.log_artifact(model_path)
+
+
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    sweep_init()
