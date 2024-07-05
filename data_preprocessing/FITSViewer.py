aff6268aad9e3432c4846398beebadc2dd8d87a5
iam37 <isaac.moskowitz@yale.edu>
2024-07-05 10:20:20 -0400

    added new seeing limited dual AGN training images :D

--- data_preprocessing/FITSViewer.py
+++ data_preprocessing/FITSViewer.py
@@ -17,6 +17,7 @@ import ipywidgets as widgets
 from IPython.display import display, clear_output
 from matplotlib.backend_bases import MouseButton
 import glob
+import shutil
 
 def cropND(img, bounding):
     start = tuple(map(lambda a, da: a//2-da//2, img.shape, bounding))
@@ -38,7 +39,7 @@ def load_fits_images(data_path, target_shape=(94, 94)):
 
     return training_images, image_files
 def display_images_with_buttons(training_images, image_files):
-    num_images = min(len(training_images), 25)
+    num_images = min(len(training_images), 49)
     random_offset_selection = np.random.choice(len(training_images), size=num_images, replace=False)
 
     buttons = []
@@ -49,11 +50,21 @@ def display_images_with_buttons(training_images, image_files):
         img_path = image_files.pop(random_offset_selection[idx])  # Remove from image_files
         training_images.pop(random_offset_selection[idx])  # Remove from training_images
 
+        # Revision to move the images to the "rubbish" catagory for further training.
+        """
         if os.path.exists(img_path):
             os.remove(img_path)
             b.style.button_color = 'red'
             b.description = f'{img_path}'
-            print(f"Deleted: {img_path}")
+            print(f"Deleted: {img_path}")"""
+        move_filepath = "data_preprocessing/training_datasets/rubbish_images/train_data/"
+        if not os.path.exists(move_filepath):
+            os.makedirs(move_filepath)
+        if os.path.exists(img_path):
+            shutil.move(img_path, move_filepath)
+            b.style.button_color = 'turquoise'
+            b.description = f'moved to {move_filepath}'
+            print(f"Moved: {img_path} to {move_filepath}")
 
     for i in range(num_images):
         idx = random_offset_selection[i]
@@ -75,8 +86,8 @@ def display_images_with_buttons(training_images, image_files):
 
         grid.append(widgets.VBox([button, output]))
 
-    grid_layout = widgets.GridspecLayout(5, 5, height='auto')
+    grid_layout = widgets.GridspecLayout(7, 7, height='auto')
     for i, item in enumerate(grid):
-        grid_layout[i // 5, i % 5] = item
+        grid_layout[i // 7, i % 7] = item
 
     display(grid_layout)
